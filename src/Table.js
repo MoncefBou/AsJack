2383f789aaa7b027a9970a604fab488354e9ce7d
Moncef <moncefboughal@hotmail.fr>
2021-04-27 12:01:52 +0200

    render conditionnel

--- src/Table.js
+++ src/Table.js
@@ -2,9 +2,14 @@ import React from "react";
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from './components/Button.jsx'
-import Cartes from "./views/Cartes.jsx";
-import StartGame from './views/StartGame.jsx';
-import { rndCarte, transformCardIntoInt } from './utils/cardsUtils.js';
+import Cartes from "./views/Cartes";
+import StartGame from './views/StartGame.jsx'
+
+const cardArray = [
+  "KS", "QS", "JS", "AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "0S",
+  "KD", "QD", "JD", "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "0D",
+  "KH", "QH", "JH", "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "0H",
+  "KC", "QC", "JC", "AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "0C"];
 
 
 class Table extends React.Component {
@@ -23,27 +28,24 @@ class Table extends React.Component {
     }
   }
 
-  // Lorsque l'on appuie sur le boutton Give ça donne une carte au player
-  onClickGive = () => {
-    const cardSelected = rndCarte()
-    const valueCarte = transformCardIntoInt(cardSelected.split("")[0])
-    const totalPlayerValue = this.state.counterPlayer + valueCarte
+  // Génére un index au hasard du tableau cardArray
+  rndCarte() {
+    let rndNumTemp = Math.floor(Math.random() * 52);
 
-    this.setState({
-      counterPlayer: totalPlayerValue,
-      playerCardList: [...this.state.playerCardList, cardSelected]
-    })
+    let rndCarteTemp = cardArray[rndNumTemp];
+
+    return rndCarteTemp
   }
 
   // Lorsque l'on appuie sur le bouton Stop ça génére les cartes du dealer,
   // et ça compare le jeu du dealer et du player,
   // ça met fin au jeu en annonçant le gagnant.
   onClickStop = () => {
-    const cardSelectedDealer = rndCarte()
-    const cardSelectedDealer2 = rndCarte()
+    const cardSelectedDealer = this.rndCarte()
+    const cardSelectedDealer2 = this.rndCarte()
 
-    const valueCarteDealer = transformCardIntoInt(cardSelectedDealer.split("")[0])
-    const valueCarteDealer2 = transformCardIntoInt(cardSelectedDealer2.split("")[0])
+    const valueCarteDealer = this.transformCardIntoInt(cardSelectedDealer.split("")[0])
+    const valueCarteDealer2 = this.transformCardIntoInt(cardSelectedDealer2.split("")[0])
 
     const cardsDealer = [cardSelectedDealer, cardSelectedDealer2]
 
@@ -55,8 +57,8 @@ class Table extends React.Component {
     }
 
     while (dealerValue < 17) {
-      const cardSelectedDealer = rndCarte()
-      const valueCarteDealer = transformCardIntoInt(cardSelectedDealer.split("")[0])
+      const cardSelectedDealer = this.rndCarte()
+      const valueCarteDealer = this.transformCardIntoInt(cardSelectedDealer.split("")[0])
 
       cardsDealer.push(cardSelectedDealer)
 
@@ -98,13 +100,35 @@ class Table extends React.Component {
     })
   }
 
+  // Lorsque l'on appuie sur le boutton Give ça donne une carte au player
+  onClickGive = () => {
+    const cardSelected = this.rndCarte()
+    const valueCarte = this.transformCardIntoInt(cardSelected.split("")[0])
+    const totalPlayerValue = this.state.counterPlayer + valueCarte
+
+    this.setState({
+      counterPlayer: totalPlayerValue,
+      playerCardList: [...this.state.playerCardList, cardSelected]
+    })
+  }
+
+  // Cette méthode donne la valeur de la carte tirée par le player ou dealer.
+  transformCardIntoInt(cardValue) {
+
+    if (cardValue === "K" || cardValue === "Q" || cardValue === "J" || cardValue === "A" || cardValue === "0") {
+      cardValue = "10"
+    }
+
+    return parseInt(cardValue)
+  }
+
   // Lorsque l'on appuie sur le boutton Start, cette méthode tire 2 cartes pour le player et lance le jeu.
   startGame = () => {
-    const cardSelected = rndCarte()
-    const cardSelected2 = rndCarte()
+    const cardSelected = this.rndCarte()
+    const cardSelected2 = this.rndCarte()
 
-    const valueCarte = transformCardIntoInt(cardSelected.split("")[0])
-    const valueCarte2 = transformCardIntoInt(cardSelected2.split("")[0])
+    const valueCarte = this.transformCardIntoInt(cardSelected.split("")[0])
+    const valueCarte2 = this.transformCardIntoInt(cardSelected2.split("")[0])
 
     const firstPlayerValue = valueCarte + valueCarte2
 
@@ -123,19 +147,15 @@ class Table extends React.Component {
       <div className="playGame" style={{ height: '100vh', position: 'relative' }}>
 
         <h1 style={{ color: '#feb236', textAlign: 'center' }}>Black Jack</h1>
-        <div style={{ height: "636px", width: "1454px" }} className="d-flex flex-column justify-content-around">
 
-          <Cartes key={"dealer"} cardList={this.state.dealerCardList} />
+        <Cartes key={"dealer"} cardList={this.state.dealerCardList} />
 
-          <div style={{height: "96px"}}>
-            {this.state.endGame && (<div className='winlost'>
-              <h1>Winner is {this.state.nameOfWinner}</h1>
-            </div>)}
-          </div>
+        {this.state.endGame && (<div className='winlost'>
+          <h1>Winner is {this.state.nameOfWinner}</h1>
+        </div>)}
 
-          <Cartes className="align-self-end" key={"player"} cardList={this.state.playerCardList} />
+        <Cartes key={"player"} cardList={this.state.playerCardList} />
 
-        </div>
         <div style={{ bottom: '20px', position: 'absolute' }} className="row col-6 offset-3 flex d-flex justify-content-between">
 
           <Button onClick={this.onClickGive} bcolor="#0d6efd"> Give </Button>
@@ -156,9 +176,9 @@ class Table extends React.Component {
     } else {
       return (
         <>
-          {this.renderGame()}
+        {this.renderGame()}
         </>
-      )
+        )
     }
   }
 }
